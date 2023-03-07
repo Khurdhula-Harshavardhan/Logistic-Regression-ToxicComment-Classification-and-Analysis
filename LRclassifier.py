920d2f41083f02f883da5d3b6d99953b38ace89f
Harsha <khurdhulaharshavardhan@gmail.com>
2023-03-06 19:49:53 -0500

    closing enhancments.

--- LRclassifier.py
+++ LRclassifier.py
@@ -40,7 +40,7 @@ class LRclassifier():
                 self.data_frame = self.data_frame.drop(columns=["id", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]) #drop the irrelevant coloumns\
             else:
                 self.data_frame = pd.DataFrame(self.data_frame["comment_text"], columns=["comment_text"])
-            print("[INFO] Read the CSV file, and extracted the relevant dataframe.")
+            print("[INFO]    Read the CSV file, and extracted the relevant dataframe.")
         except Exception as e:
             print("[ERR] The following error occured while trying to read the Train csv file: "+str(e))
 
@@ -70,7 +70,7 @@ class LRclassifier():
             print("[PROCESS] Normalizing the comment_text, from the data frame.")
             self.data_frame["comment_text"] = self.data_frame["comment_text"].apply(self.normalize_single_comment)
             #print(self.data_frame)
-            print("[INFO] Normalized the data successfully!")
+            print("[INFO]    Normalized the data successfully!")
         except Exception as e:
             print("[ERR] The following error occured while tryig to normalize the data_frame: "+str(e))
 
@@ -81,17 +81,17 @@ class LRclassifier():
         try:
             print("[PROCESS] Creating Feature vectors for the data, please wait this might take some time.")
             toxicData = self.data_frame.loc[self.data_frame["toxic"] == 1]
-            print("[INFO] Extracted toxic comments.")
+            print("[INFO]    Extracted toxic comments.")
 
             nontoxicData = self.data_frame.loc[self.data_frame["toxic"] == 0][:60000]
-            print("[INFO] Extracted 60,000 nontoxic comments.")
+            print("[INFO]    Extracted 60,000 nontoxic comments.")
             
             self.data_frame = pd.concat([toxicData, nontoxicData])
             x_sample = [ str(x) for x in self.data_frame["comment_text"]]
             self.y= self.data_frame["toxic"]
             print("[PROCESS] Transforming the comments into Feature Vectors please wait this migth take time!")
             self.X = self.vectorizer.fit_transform(x_sample)
-            print("[INFO] Feature Vectors created successfully!")
+            print("[INFO]    Feature Vectors created successfully!")
         except Exception as e:
             print("[ERR] The following error occured while trying to create feature vectors: "+str(e))
 
@@ -100,19 +100,19 @@ class LRclassifier():
         This model tries to load a previously trained classifier to save time.,
         """
         try:
-            print("[INFO] Trying to load a previously trained model!")
+            print("[INFO]    Trying to load a previously trained model!")
             self.model = joblib.load("LRclassifier.joblib")
-            print("[INFO] Aha! A previously trained classifier exists and it has been initiated!")
+            print("[INFO]    Aha! A previously trained classifier exists and it has been initiated!")
             return True
         except:
-            print("[INFO] Sorry! No previously Trained model could be found, will create a new one!")
+            print("[INFO]    Sorry! No previously Trained model could be found, will create a new one!")
 
     def save_model(self, model):
         """
         This method saves a model that was just trained newly.
         """
         try:
-            print("[INFO] Saving the newly trained model as: LRclassifier.joblib within the same directory.")
+            print("[INFO]    Saving the newly trained model as: LRclassifier.joblib within the same directory.")
             joblib.dump(model, "LRclassifier.joblib")
         except Exception as e:
             print("[ERR] The following error occured while trying to save the model: "+str(e))
@@ -129,7 +129,7 @@ class LRclassifier():
             self.read_file(path_to_train_file) #reads the file and extracts relevant information.
             self.normalize() #normalize the data
             self.create_word_embeddings() #create the feature vectors.
-            print("[INFO] Defining the Threshold to be: 0.3 Manually.")
+            print("[INFO]    Defining the Threshold to be: 0.3 Manually.")
             print("[PROCESS] Splitting this data, inorder get model metrics!")
             self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.4, random_state=42)
             self.threshold = 0.3
@@ -138,12 +138,12 @@ class LRclassifier():
             else:
                 
                 print("[PROCESS] Creating a new instance of Logistic Regression.")
-                print("[INFO] Extending the max iterations to 1000 from 100 to this instance.")
+                print("[INFO]    Extending the max iterations to 1000 from 100 to this instance.")
                 self.model = LogisticRegression(max_iter=1000)
-                print("[INFO] Created a new instance of Logistic Regression Class.")
+                print("[INFO]    Created a new instance of Logistic Regression Class.")
                 print("[PROCESS] Training LRmodel, please wait this might take some time.")
                 self.model = self.model.fit(self.X_train, self.y_train)
-                print("[INFO] Model has been trained Successfully, returning the instance of this model!")
+                print("[INFO]    Model has been trained Successfully, returning the instance of this model!")
                 self.save_model(self.model)
                 return self.model
         except Exception as e:
@@ -163,7 +163,7 @@ class LRclassifier():
             _accuracy = accuracy_score(predictions, self.y_test)
             _precision, _recall, _F1_score, _ = precision_recall_fscore_support(self.y_test, predictions, average='binary')
            
-            print("[INFO] Here are the LRmodel metrics: ")
+            print("[INFO]    Here are the LRmodel metrics: ")
             print("[METRIC] Accuracy Score: %f"%(_accuracy))
             print("[METRIC] Precision: %f"%(_precision))
             print("[METRIC] F1-Score: %f"%(_F1_score))
@@ -200,7 +200,7 @@ class LRclassifier():
             if LRmodel is None: #check if a model has been given as input else throw an exception asking for one.
                 raise Exception("No Logisitic Regression model has been passed to the test method, please provide the appropriate parameters.")
             
-            print("[INFO] Analyzing the model metrics please wait.")
+            print("[INFO]    Analyzing the model metrics please wait.")
             self.traditional_test(LRmodel) #Aquire model metrics.
 
             print("-"*50)
@@ -211,21 +211,24 @@ class LRclassifier():
             print("[PROCESS] Normalizing the Comments please wait!")
 
             self.normalize() #normalize the comments within the test file.
-            # print(self.data_frame.head())
+            # print(self.data_frame.head())6
             result_data = list()
             print("[PROCESS] Performing classifications for testset please wait this might take a while!")
-            for comment in self.data_frame["comment_text"][:10]:
+            total = len(self.data_frame["comment_text"])
+            counter = 0
+            for comment in self.data_frame["comment_text"]:
+                if comment is None:
+                    continue
                 prediction = self.make_prediction(comment, LRmodel)
                 result_data.append(prediction)
+                counter = counter + 1
+                progress = "[PROCESS] Progess: " + str(int((counter/total)*100)) + "% DONE."
+                print(progress, end="\r")
             data_frame= pd.DataFrame(result_data, columns=["comment_text", "toxicity_score", "toxic"])
-            print("[INFO] ALL COMPUTATIONS DONE.")
-            print("[INFO] Writing results to _output.csv within the same directory!")
+            print("[INFO]    ALL COMPUTATIONS DONE.")
+            print("[INFO]    Writing results to _output.csv within the same directory!")
             data_frame.to_csv("_output.csv")
             print("[SUCCESS] ALL RESULTS HAVE BEEN DUMPED TO: _output.csv")
         except Exception as e:
             print("[ERR] The following error while trying to perform test on the LRmodel: "+str(e))
 
-
-obj = LRclassifier()
-LR_model = obj.train_LR_model()
-obj.test_LR_model(LRmodel = LR_model)
